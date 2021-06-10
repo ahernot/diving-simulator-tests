b4fae21f9cb9ffb60abe607255b5099baeb166ce
ahernot <70817470+ahernot@users.noreply.github.com>
2021-06-10 13:07:53 +0200

    create BiomeGeneratorEditor v1.0

---
+++ Assets/Editor/BiomeGeneratorEditor.cs
@@ -0,0 +1,38 @@
+﻿/*
+ Copyright Anatole Hernot, 2021
+ Licensed to CRC Mines ParisTech
+ All rights reserved
+
+ BiomeGeneratorEditor v1.0
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (BiomeGenerator))]
+public class BiomeGeneratorEditor : Editor
+{
+    // Override default inspector
+    public override void OnInspectorGUI()
+    {
+        // Fetch target
+        BiomeGenerator biomeGenerator = (BiomeGenerator)target;
+
+        // Draw default inspector
+        if (DrawDefaultInspector())
+        {
+            DrawDefaultInspector();
+        }
+
+        EditorGUILayout.Space();
+
+        // Draw Generate button
+        if (GUILayout.Button ("Generate Biomes"))
+        {
+            biomeGenerator.Generate();
+        }
+
+    }
+}
