94b6e2e940dde8e50426177081a2d6bcb2214e54
ahernot <70817470+ahernot@users.noreply.github.com>
2021-06-17 09:14:03 +0200

    add editor scripts

---
+++ Assets/Editor/TerrainChunkManagerEditor.cs
@@ -0,0 +1,39 @@
+﻿/*
+ Copyright Anatole Hernot, 2021
+ Licensed to CRC Mines ParisTech
+ All rights reserved
+ 
+ TerrainChunkManagerEditor v1.1
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (TerrainChunkManager))]
+public class TerrainChunkManagerEditor : Editor
+{
+    // Override default inspector
+    public override void OnInspectorGUI()
+    {
+        // Fetch target
+        TerrainChunkManager terrainChunkManager = (TerrainChunkManager)target;
+
+        // Draw default inspector
+        if (DrawDefaultInspector())
+        {
+            DrawDefaultInspector();
+        }
+
+        EditorGUILayout.Space();
+
+        // Draw Generate button
+        if (GUILayout.Button ("Generate Chunks"))
+        {
+            terrainChunkManager.DestroyChunks();
+            terrainChunkManager.GenerateChunks();
+        }
+
+    }
+}
