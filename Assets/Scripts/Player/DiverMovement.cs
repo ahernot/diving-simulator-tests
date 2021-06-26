90b537104194e6b3915ae4f01e16e29527a19b87
ahernot <70817470+ahernot@users.noreply.github.com>
2021-06-26 20:53:32 +0200

    Update DiverMovement.cs

--- Assets/Scripts/Player/DiverMovement.cs
+++ Assets/Scripts/Player/DiverMovement.cs
@@ -14,12 +14,12 @@ public class DiverMovement : MonoBehaviour
 {
     [Header("Player")]
     public CharacterController controller;
-    [Tooltip("Main FPV camera")]
     public Camera viewCamera;
 
     public GameObject handLeft;
     public GameObject handRight;
 
+    public float deadZoneAsymmmetry = 0.25f;
     public float deadZoneVal = 0.00001f;
 
     Vector3 handLeftPosition;
@@ -29,7 +29,8 @@ public class DiverMovement : MonoBehaviour
 
     [Header("Water Parameters")]
     [Tooltip("Water current direction vector (not normalized) — requires force generation on update")]
-    public Vector3 waterCurrentDirection = new Vector3 (1, 0, 0);
+    [Range(0, 360)]
+    public float waterCurrentDirection = 0f;
     [Tooltip("Water current velocity — requires force generation on update")]
     public float waterCurrentVelocity = 0.15f;
     Vector3 waterVelocity = new Vector3();
@@ -38,11 +39,14 @@ public class DiverMovement : MonoBehaviour
 
     [Header("Force Parameters")]
     public float movementForceMultiplier = 20f;
-    public float movementTorqueMultiplier = 5f;
+    public float movementTorqueMultiplier = 25f;
     public float dragForceMultiplier = 4f;
     public float dragTorqueMultiplier = 5f;
     [Tooltip("Vertical force multiplier (gravity + buoyancy) — requires force generation on update")]
     public float verticalForceMultiplier = -0.8f;
+
+    public float maxVelocity = 10f;
+    public float maxAngularVelocity = 5f;
 
     //----------//
 
@@ -60,25 +64,53 @@ public class DiverMovement : MonoBehaviour
     Vector3 verticalForce = new Vector3();
 
 
+    // ----- START ----- //
     void Start ()
     {
-        // Initialise motion variables
-        this.acceleration = new Vector3();
-        this.velocity = new Vector3();
-
-        // Generate forces
-        this.RegenerateForces();
+        // Calculate vertical force
+        this.CalculateVerticalForce();
+        
+        // Calculate water velocity
+        this.CalculateWaterVelocity();
 
         // Initialise hand position memory
         this.handLeftPosition = (this.handLeft).transform.position;
         this.handRightPosition = (this.handRight).transform.position;
     }
 
+    // ----- UPDATE ----- //
     void Update ()
     {
         this.UpdateMovement();
+        this.TriggerResetMovement();
     }
 
+
+    // ----- UPDATE MOVEMENT ----- //
+    void UpdateMovement ()
+    {
+        // Calculate forces
+        this.CalculateMovementForces();
+        this.CalculateDragForces();
+
+        // Integrate motion
+        this.acceleration = this.dragForce + this.verticalForce + this.movementForce;
+        this.velocity += this.acceleration * Time.deltaTime;
+        if (this.velocity.magnitude > this.maxVelocity) { this.velocity = this.maxVelocity * Vector3.Normalize (this.velocity); }
+        this.angularAcceleration = this.dragTorque + this.movementTorque;
+        this.angularVelocity += this.angularAcceleration * Time.deltaTime;
+        if (this.angularVelocity.magnitude > this.maxAngularVelocity) { this.angularVelocity = this.maxAngularVelocity * Vector3.Normalize (this.angularVelocity); }
+
+        Vector3 positionDelta = this.velocity * Time.deltaTime;
+        Vector3 rotationDelta = this.angularVelocity * Time.deltaTime;
+
+        // Move player
+        controller.Move (positionDelta);
+        transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles + rotationDelta);
+    }
+
+
+    // ----- CALCULATE FORCES ----- //
     void CalculateMovementForces ()
     {
         // Calculate hand position deltas
@@ -94,9 +126,9 @@ public class DiverMovement : MonoBehaviour
         {
             float movementBalance = rightHandDirection.magnitude / cumulativePositionDelta - 0.5f;
 
-            if (Mathf.Abs(movementBalance) > asymmetryDeadZone)
+            if (Mathf.Abs(movementBalance) > deadZoneAsymmmetry)
             {
-                this.angularAcceleration = new Vector3 (
+                this.movementTorque = new Vector3 (
                     0f,
                     -1f * movementBalance * this.movementTorqueMultiplier * this.viewCamera.transform.up,
                     0f
@@ -130,48 +162,41 @@ public class DiverMovement : MonoBehaviour
         this.dragTorque = -1 * this.dragTorqueMultiplier * this.angularVelocity;
     }
 
+    void CalculateVerticalForce ()
+    {
+        this.verticalForce = new Vector3 (
+            0f,
+            this.verticalForceMultiplier,
+            0f
+        );
+    }
+
+    void CalculateWaterVelocity ()
+    {
+        float waterCurrentDirectionRadians = this.waterCurrentDirection / 180 * Mathf.PI;
+        
+        Vector3 waterDirectionVector = new Vector3 (
+            Mathf.Cos (waterCurrentDirectionRadians),
+            0f,
+            Mathf.Sin (waterCurrentDirectionRadians)
+        );
+
+        this.waterVelocity = this.waterCurrentVelocity * waterDirectionVector;
+    }
 
 
-    void UpdateResetMovement ()
+    // ----- RESET MOVEMENT ----- //
+    public void ResetMovement ()
+    {
+        this.acceleration = Vector3.zero;
+        this.velocity = Vector3.zero;
+    }
+
+    void TriggerResetMovement ()
     {
         // Get Backspace input
         if (Input.GetKeyDown(KeyCode.Backspace)) {
             this.ResetMovement();
         }
-    }
-
-    void UpdateMovement ()
-    {   
-        // Update inputs
-        this.UpdateResetMovement();
-
-        // Calculate forces
-        this.CalculateMovementForces();
-        this.CalculateDragForces();
-
-        // Integrate motion
-        this.acceleration = this.dragForce + this.verticalForce + this.movementForce;
-        this.velocity += this.acceleration * Time.deltaTime;
-        this.angularAcceleration = this.dragTorque + this.movementTorque;
-        this.angularVelocity += this.angularAcceleration * Time.deltaTime;
-
-        // Move player
-        controller.Move (this.velocity * Time.deltaTime);
-    }
-
-
-    public void RegenerateForces ()
-    {
-        // Compute vertical force vector
-        this.verticalForce = new Vector3 (0f, this.verticalForceMultiplier, 0f);
-
-        // Compute water velocity vector
-        this.waterVelocity = this.waterCurrentVelocity * Vector3.Normalize (this.waterCurrentDirection);
-    }
-
-
-    public void ResetMovement ()
-    {
-        this.velocity = Vector3.zero;
     }
 }
